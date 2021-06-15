35bc6bc26a6835eefd3c5abe78c6e773bb816bd6
Siek Ming Jun <siekmingjun@hotmail.com>
2021-06-15 22:55:11 +0800

    Updated Merge

--- lib/widgets/NearbyClinicPanel.dart
+++ lib/widgets/NearbyClinicPanel.dart
@@ -1,41 +1,87 @@
+import 'package:clinic_q/controllers/clinic_controller.dart';
+import 'package:clinic_q/model/clinic.dart';
+import 'package:clinic_q/utils/size_helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:clinic_q/utils/constants.dart';
+import 'package:get/get.dart';
 
 class NearbyClinicPanel extends StatelessWidget {
   final ScrollController controller;
+  final clinicController = Get.find<ClinicController>();
 
-  const NearbyClinicPanel({
+  NearbyClinicPanel({
     Key? key,
     required this.controller,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final items = List<String>.generate(10, (i) => "Item $i");
+    List<Clinic> clinicList = clinicController.clinicList;
 
     return Column(
       children: [
         SizedBox(
-          height: 30.0,
+          height: 10.0,
         ),
         Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: kUnselectedColor,
+          ),
           height: 5.0,
           width: 30.0,
-          color: kPrimaryBtnColor,
         ),
         Expanded(
           child: ListView.builder(
               controller: controller,
-              itemCount: items.length,
+              itemCount: clinicList.length,
               itemBuilder: (context, index) {
-                return Card(
-                    child: ListTile(
-                  onTap: () {},
-                  title: Text(
-                    '${items[index]}',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ));
+                return Column(
+                  children: [
+                    Card(
+                      child: ListTile(
+                        onTap: () {},
+                        // leading: Text(
+                        //   'Leading',
+                        //   style: TextStyle(color: Colors.black),
+                        // ),
+                        leading: FittedBox(
+                          fit: BoxFit.contain,
+                          child: Container(
+                            width: displayWidth(context) * 0.6,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${clinicList[index].clinicName}',
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                                Text(
+                                  '${clinicList[index].streetName}',
+                                  style: TextStyle(color: kUnselectedColor),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        trailing: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Current Queue",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                            Text(
+                              "5/25",
+                              style: TextStyle(color: kUnselectedColor),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    // Container(height: displayHeight(context) * 0.01),
+                  ],
+                );
               }),
         ),
       ],
